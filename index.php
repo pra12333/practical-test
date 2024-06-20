e7d8c0fc7ce275e04ea63259914d8211413039cb
pra12333 <praveen60fernando@gmail.com>
2024-06-20 20:18:01 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Internship Applicants</title>
+</head>
+<body>
+    Welcome <?php echo $_POST["name"]; ?><br>
+    <?php echo $_POST["email"]; ?><br>
+    <?php echo $_POST["sex"];?><br>
+    <?php echo $_POST["phone"];?><br>
+    <?php echo $_POST["birthday"];?><br>
+    <?php echo $_POST["job"];?><br>
+    
+</body>
+</html>
